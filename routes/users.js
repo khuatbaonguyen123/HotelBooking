b48a99cb70a121d3c92f5f2f4303e5dcfc8b59ef
thaombll <tailieufromthao@gmail.com>
2024-04-22 23:17:16 +0700

    security(lag)

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db=require('../database');
 const bcrypt=require('bcrypt');
-
+const puppeteer = require('puppeteer');
 function isLoggedOut(req,res,next){
     if (req.session.userId)
         res.redirect('/booking');
@@ -16,87 +16,134 @@ function isLoggedIn(req,res,next)
     else res.redirect('/loginform');
 }
 
+async function getCheckLink(Input) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://safeweb.norton.com/');
+    await page.waitForSelector("input[type='url']", { timeout: 3000 });
+    const input = await page.$("input[type='url']");
+    await input.type(Input);
+    await input.press('Enter');
+    try {
+        await page.waitForNavigation()
+        const ratingElement = await page.$("p.rating-label.xl-body-text-bold");
+        const ratingText = await page.evaluate(ratingElement => ratingElement.textContent, ratingElement);
+        await browser.close();
+        return ratingText;
+        
+    }
+    catch {
+        await browser.close();
+        return "Not Link";
+    }
+}
 
 router.post('/signup', async (req,res)=>{
-    const {email,password} = req.body;
+    const {fname, lname, email,password} = req.body;
     let hashedPassword= await bcrypt.hash(password,10);
-    db.beginTransaction((err) => {
-        if (err) throw err;
-        db.query(`SELECT * FROM account WHERE email='${email}'`, (err, results) => {
-            if (err) {
-                db.rollback(() => {
-                    throw err;
-                });
-            }
-            if (results.length > 0) {
-                req.flash('error', 'Email already registered');
-                res.redirect('/signupform');
-                db.rollback();
-            } else {
-                db.query(`INSERT INTO account (email,password) VALUES ('${email}','${hashedPassword}')`, (err) => {
-                    if (err) {
-                        db.rollback(() => {
-                            throw err;
-                        });
-                    }
-                    const { fname, lname, phone, dob } = req.body;
-                    db.query(`SELECT id FROM account WHERE email='${email}'`, (err, results) => {
+    let checkfName = await getCheckLink(fname);
+    console.log("fname:" + checkfName);
+    if (checkfName === "Not Link" || checkfName === "Safe") {
+        let checklName = await getCheckLink(lname);
+        console.log("lname:" + checklName);
+        if (checklName === "Not Link" || checklName === "Safe") {
+            let checkPassword = await getCheckLink(password);
+            console.log(checkPassword);
+            if ((checkPassword === "Not Link" || checkPassword === "Safe") ) {
+                db.beginTransaction((err) => {
+                    if (err) throw err;
+                    db.query(`SELECT * FROM account WHERE email='${email}'`, (err, results) => {
                         if (err) {
                             db.rollback(() => {
                                 throw err;
                             });
                         }
-                        const id = results[0].id;
-                        db.query(`INSERT INTO booker VALUES (${id},'${fname}','${lname}','${dob}','${phone}')`, (err) => {
-                            if (err) {
-                                db.rollback(() => {
-                                    throw err;
-                                });
-                            }
-                            db.commit((err) => {
+                        if (results.length > 0) {
+                            req.flash('error', 'Email already registered');
+                            res.redirect('/signupform');
+                            db.rollback();
+                        } else {
+                            db.query(`INSERT INTO account (email,password) VALUES ('${email}','${hashedPassword}')`, (err) => {
                                 if (err) {
                                     db.rollback(() => {
                                         throw err;
                                     });
                                 }
-                                res.redirect('/index');
+                                const { fname, lname, phone, dob } = req.body;
+                                db.query(`SELECT id FROM account WHERE email='${email}'`, (err, results) => {
+                                    if (err) {
+                                        db.rollback(() => {
+                                            throw err;
+                                        });
+                                    }
+                                    const id = results[0].id;
+                                    db.query(`INSERT INTO booker VALUES (${id},'${fname}','${lname}','${dob}','${phone}')`, (err) => {
+                                        if (err) {
+                                            db.rollback(() => {
+                                                throw err;
+                                            });
+                                        }
+                                        db.commit((err) => {
+                                            if (err) {
+                                                db.rollback(() => {
+                                                    throw err;
+                                                });
+                                            }
+                                            res.redirect('/index');
+                                        });
+                                    });
+                                });
                             });
-                        });
+                        }
                     });
                 });
             }
-        });
-    });
-})
-
-router.post('/signin',(req,res)=>{
-    const {email,password} = req.body;
-    if (!email | !password)
-        res.status(404).json({message: 'Please enter all fields'});
-    else{
-        db.query(`select * from account where email='${email}' and type_of_account = 'booker'`,(err,results)=>{
-            if (err) throw err;
-            if (results.length>0){
-                const user=results[0];
-                bcrypt.compare(password,user.password, (err,isMatch)=>{
-                    if (err) throw err;
-                    if (isMatch){
-                        req.session.userId=user.id;
-                        res.redirect('/index');
-                    } 
-                    else {
-                        req.flash('error','Password is not correct');
-                        res.redirect('/loginform')
-                    }
-                })
-            }
             else {
-                req.flash('error','Email is not registered');
-                res.redirect('/loginform')
+                return res.status(400).json({ error: 'Password not valid.' });
             }
-        })
+        }
+        else {
+            return res.status(400).json({ error: 'last name not valid.' });
+        }
+    }
+    else {
+        return res.status(400).json({ error: 'First name not valid.' });
     }
 })
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(404).json({ message: 'Please enter all fields' });
+        return; 
+    }
+    let checkPassword = await getCheckLink(password);
+    console.log(checkPassword);
+    if ((checkPassword === "Not Link" || checkPassword === "Safe")) {
+        db.query(`SELECT * FROM account WHERE email='${email}' AND type_of_account = 'booker'`, (err, results) => {
+            if (err) throw err;
+            if (results.length > 0) {
+                const user = results[0];
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) throw err;
+                    if (isMatch) {
+                        req.session.userId = user.id;
+                        res.redirect('/index');
+                    } else {
+                        req.flash('error', 'Password is not correct');
+                        res.redirect('/loginform');
+                    }
+                });
+            } else {
+                req.flash('error', 'Email is not registered');
+                res.redirect('/loginform');
+            }
+        });
+    }
+    else {
+        return res.status(400).json({ error: 'Password not valid.' });
+    }
+});
 
 router.get('/profile', isLoggedIn, (req, res) => {
     db.query(`select * 
