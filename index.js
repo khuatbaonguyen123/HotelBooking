20901de378b5820591666d7927e09c2e724251f8
Do-Trang <thutrang25062k4@gmail.com>
2024-04-25 15:56:31 +0700

    css

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
-const express = require('express');
-const ejs = require('ejs');
-require('dotenv').config();
-const session=require('express-session');
-const flash=require('connect-flash');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const ejs = require("ejs");
+require("dotenv").config();
+const session = require("express-session");
+const flash = require("connect-flash");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const clientRedis = require("./connect_redis");
 const RedisStore = require("connect-redis").default;
-const usersRouter = require('./routes/users');
-const generalRouter = require('./routes/general');
-const bookingRouter = require('./routes/booking');
-const adminRouter = require('./routes/admin');
-const chatRouter = require('./routes/chatting');
+const usersRouter = require("./routes/users");
+const generalRouter = require("./routes/general");
+const bookingRouter = require("./routes/booking");
+const adminRouter = require("./routes/admin");
+const chatRouter = require("./routes/chatting");
 
-const chatDemo = require('./routes/chatdemo');
+const chatDemo = require("./routes/chatdemo");
 
-const ratingRouter = require('./routes/rating');
-
-
-const ratingRouter2 = require('./routes/rating2');
-const ratingRouter3 = require('./routes/rating3');
-const ratingRouter4 = require('./routes/rating4');
-const ratingRouter5 = require('./routes/rating5');
-const ratingRouter6 = require('./routes/rating6');
-
+const ratingRouter = require("./routes/rating");
 
 const app = express();
 
-
-mongoose.connect(process.env.MONGOLOCAL_URL, {
+mongoose
+  .connect(process.env.MONGOLOCAL_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch(error => {
-    console.error('Error connecting to MongoDB:', error);
-});
-
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 // const clientRedis = Redis.createClient(); //default localhost
 
@@ -51,29 +42,34 @@ mongoose.connect(process.env.MONGOLOCAL_URL, {
 // });
 
 const oneDay = 1000 * 60 * 60 * 24;
-app.use(session({
+app.use(
+  session({
     secret: "thisismysecretkey",
-    store: new RedisStore({host: 'localhost', port: 6379, client: clientRedis}),
-    saveUninitialized:true,
+    store: new RedisStore({
+      host: "localhost",
+      port: 6379,
+      client: clientRedis,
+    }),
+    saveUninitialized: true,
     cookie: { maxAge: oneDay },
     resave: false,
-}));
+  })
+);
 
 app.use(flash());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.set('views', 'pages');
+app.set("views", "pages");
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.json());
 
-app.use((req,res,next)=>{
-  res.locals.session=req.session;
+app.use((req, res, next) => {
+  res.locals.session = req.session;
   next();
-})
+});
 
 app.use(bodyParser.json());
-
 
 app.use(generalRouter);
 app.use(usersRouter);
@@ -81,27 +77,21 @@ app.use(bookingRouter);
 app.use(adminRouter);
 app.use(chatRouter);
 app.use(ratingRouter);
-app.use(ratingRouter2);
-app.use(ratingRouter3);
-app.use(ratingRouter4);
-app.use(ratingRouter5);
-app.use(ratingRouter6);
 
 app.use(chatDemo);
 
-app.get('/get-session', (req, res) =>{
+app.get("/get-session", (req, res) => {
   res.send(req.session);
-})
-app.get('/set-session', (req, res) => {
+});
+app.get("/set-session", (req, res) => {
   req.session.user = {
     username: "Tips Java",
     age: 12,
-    email: "commeo123@gmail.com"
-  }
-  res.send('Set OK');
-})
-
+    email: "commeo123@gmail.com",
+  };
+  res.send("Set OK");
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
--- routes/rating.js
+++ routes/rating.js
@@ -1,70 +1,67 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Rating = require('../model_mongodb/dbmongo.js');
-const db = require('../database');
-const cli = require('../connect_redis.js');
-let cnt = 0;
+const Rating = require("../model_mongodb/dbmongo.js");
+const db = require("../database");
 
+router.post("/submit", async (req, res) => {
+  const { rating } = req.body;
+  const { id } = req.query;
+  console.log(id);
 
-router.post('/submit', async (req, res) => {
-    const { rating } = req.body; 
+  if (!rating) {
+    return res.status(400).json({ error: "Rating is a required field." });
+  }
 
-    if (!rating) {
-        return res.status(400).json({ error: 'Rating is a required field.' });
-    }
-
-    const dbquery = "SELECT DISTINCT booker_id FROM reservation re JOIN room_reserved rr ON rr.reservation_id = re.id JOIN room r ON r.id = rr.room_id WHERE re.booker_id = ? AND r.type_id = 1 AND re.status =  'checkout';";
-    db.query(dbquery, [req.session.userId], async (err, results) => {
+  const dbquery =
+    "SELECT DISTINCT booker_id FROM reservation re JOIN room_reserved rr ON rr.reservation_id = re.id JOIN room r ON r.id = rr.room_id WHERE re.booker_id = ? AND r.type_id = 1 AND re.status =  'checkout';";
+  db.query(dbquery, [req.session.userId], async (err, results) => {
     if (err) {
-        // Xử lý lỗi
-        console.error('Error querying database:', err);
-        return res.status(500).json({ error: 'Internal server error' });
+      // Xử lý lỗi
+      console.error("Error querying database:", err);
+      return res.status(500).json({ error: "Internal server error" });
     }
 
     if (results.length === 0) {
-        // Trả về thông báo lỗi "Chưa đánh giá"
-        console.log('Chưa thuê phòng');
-        return res.redirect('/Assignment_r');
+      // Trả về thông báo lỗi "Chưa đánh giá"
+      console.log("Chưa thuê phòng");
+      return res.redirect(`/Assignment_r${id}`);
     }
     // Thực hiện hành động khi kết quả không rỗng
-    try{
-        for (let i = 0; i < results.length; i ++) {
-            const userId = results[i].booker_id;
-          
-            // const dateIn = results[i].date_in;
-            const newRating = await Rating.create({ rating, idUser: userId});
-            res.redirect('/Assignment_s');
-        }
-    }
-    catch {
-        console.error('Đã đánh giá', err);
-        return res.redirect('/Assignment_db');
-    }
-});
-
-});
-
-
-router.get('/data', async (req, res) => {
     try {
-        cli.zincrby('myzset', 1, '1', (err, reply) => {
-            if (err) {
-              console.error('Error incrementing score:', err);
-            }
-          
-            console.log('New score of member "room1":');
-            cli.zscore('myzset', '1', (err, reply) => {console.log(reply);})
+      for (let i = 0; i < results.length; i++) {
+        const userId = results[i].booker_id;
+
+        // const dateIn = results[i].date_in;
+        const newRating = await Rating.create({
+          idRoom: Number(id),
+          rating,
+          idUser: userId,
         });
-        const data = await Rating.aggregate([
-            { $group: { _id: '$rating', count: { $sum: 1 } } },
-            { $project: { _id: 0, stars: '$_id', count: 1 } }
-        ]);
-        res.json(data);
-    } catch (error) {
-        console.error('Error retrieving data:', error);
-        return res.status(500).json({ error: 'Internal server error' });
+        res.redirect(`/Assignment_s${id}`);
+      }
+    } catch {
+      console.error("Đã đánh giá", err);
+      return res.redirect(`/Assignment_db${id}`);
     }
+  });
 });
 
-module.exports = router; 
+router.get("/data", async (req, res) => {
+  const { id } = req.query;
+  console.log(id);
+  try {
+    const data = await Rating.aggregate([
+      { $match: { idRoom: Number(id) } },
+      { $group: { _id: "$rating", count: { $sum: 1 } } },
+      { $project: { _id: 0, stars: "$_id", count: 1 } },
+    ]);
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
