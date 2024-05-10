54b4e6c68778d0abd6f535970c9537b64082a615
khuatbaonguyen123 <anh10.14khuatbaonguyen@gmail.com>
2024-05-10 08:55:55 +0700

    Update queries

--- bookingapp_query/changeAccount.sql
+++ bookingapp_query/changeAccount.sql
@@ -1,3 +1,4 @@
+-- Break admin and booker table
 ALTER TABLE booker DROP FOREIGN KEY FK_bookerID;
 
 CREATE TABLE admin (
@@ -12,4 +13,50 @@ ADD COLUMN password varchar(255) not null;
 
 insert into admin(email,password)
 values ('admin@test.com','admin');
+
+-- trigger when delete a booker also delete all their reservations
+DELIMITER //
+
+CREATE TRIGGER delete_booker_reservation
+BEFORE DELETE ON booker
+FOR EACH ROW
+BEGIN
+	delete from reservation where booker_id = OLD.id;
+END //
+
+DELIMITER ;	
+
+-- trigger to restrict modification on date_in, date_out once inserted
+DELIMITER //
+
+CREATE TRIGGER prevent_date_modification
+BEFORE UPDATE ON reservation
+FOR EACH ROW
+BEGIN
+    IF OLD.date_in != NEW.date_in OR OLD.date_out != NEW.date_out THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot modify date_in and date_out once inserted';
+    END IF;
+END//
+
+DELIMITER ;
+
+-- restrict viec xoa phong da dat (trigger to restrict delete if the booker still exists)
+DELIMITER //
+
+CREATE TRIGGER restrict_reservation_deletion
+BEFORE DELETE ON reservation
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT 1 FROM booker WHERE id = OLD.booker_id) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot delete reservation because corresponding booker exists';
+    END IF;
+END//
+
+DELIMITER ;
+
+
+
+
 
--- bookingapp_query/querybooking.sql
+++ bookingapp_query/querybooking.sql
@@ -65,6 +65,48 @@ DO
     UPDATE reservation
     SET status = 'decline'
     WHERE status = 'pending' AND created_date < curdate();
+    
+-- trigger when delete a booker also delete all their reservations
+DELIMITER //
+
+CREATE TRIGGER delete_booker_reservation
+BEFORE DELETE ON booker
+FOR EACH ROW
+BEGIN
+	delete from reservation where booker_id = OLD.id;
+END //
+
+DELIMITER ;	
+
+-- trigger to restrict modification on date_in, date_out once inserted
+DELIMITER //
+
+CREATE TRIGGER prevent_date_modification
+BEFORE UPDATE ON reservation
+FOR EACH ROW
+BEGIN
+    IF OLD.date_in != NEW.date_in OR OLD.date_out != NEW.date_out THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot modify date_in and date_out once inserted';
+    END IF;
+END//
+
+DELIMITER ;
+
+-- restrict viec xoa phong da dat (trigger to restrict delete if the booker still exists)
+DELIMITER //
+
+CREATE TRIGGER restrict_reservation_deletion
+BEFORE DELETE ON reservation
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT 1 FROM booker WHERE id = OLD.booker_id) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot delete reservation because corresponding booker exists';
+    END IF;
+END//
+
+DELIMITER ;
 
 -- View 
 
